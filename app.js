8e49b707f72301f50f1c3ca8ee8b2eafec7cc189
Faisal <faisal@Faisals-MacBook-Pro.local>
2020-10-18 12:36:57 +1300

    make it better

---
+++ app.js
@@ -0,0 +1,30 @@
+/* lr map by @jquery404(faisal) */
+
+const express = require('express');
+const app = express();
+
+// middlewares
+app.use(express.static(__dirname));
+
+// routes
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// routes
+app.get('/dataviz', (req, res) => {
+    res.sendFile(__dirname + '/dataviz/index.html');
+});
+
+// routes
+app.get('/jigsaw', (req, res) => {
+    res.sendFile(__dirname + '/dataviz/jigsaw.html');
+});
+
+// routes
+app.get('/draw', (req, res) => {
+    res.sendFile(__dirname + '/draw/index.html');
+});
+
+// port
+server = app.listen(process.env.PORT || 3000);
